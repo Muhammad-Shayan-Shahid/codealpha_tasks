a059b6aef35ca4683ad842fe1caadef4e6607b6c
Muhammad Shayan Shahid <msshahid2305006@gmail.com>
2026-02-06 20:06:11 +0500

    Image Gallary Craeted

---
+++ Task1/script.js
@@ -0,0 +1,35 @@
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+
+let currentIndex = 0;
+
+function openLightbox(index) {
+  currentIndex = index;
+  lightbox.style.display = "flex";
+  lightboxImg.src = images[index].src;
+}
+
+function closeLightbox() {
+  lightbox.style.display = "none";
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+}
+
+function filterImages(category) {
+  images.forEach(img => {
+    if (category === "all" || img.classList.contains(category)) {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  });
+}
